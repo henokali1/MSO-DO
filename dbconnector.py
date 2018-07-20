7c913331506f439fe04023cd89106d75b3a72d86
Henok Ali <henokali1@gmail.com>
2018-07-20 10:57:07 +0400

    .

--- dbconnector.py
+++ dbconnector.py
@@ -4,7 +4,8 @@ import mysql.connector
 # Get all user info by id.
 # @returns: A dictionary with all(single user according to id) user info
 def get_user_by_id(id):
-    cnx = mysql.connector.connect(user='root', password='@tmsqe!1321', host='127.0.0.1', database='MSO')
+    cnx = mysql.connector.connect(
+        user='root', password='@tmsqe!1321', host='127.0.0.1', database='MSO')
 
     cur = cnx.cursor(dictionary=True)
 
@@ -17,20 +18,23 @@ def get_user_by_id(id):
 
 # Get a single user by email.
 def get_user_by_email(email):
-    cnx = mysql.connector.connect(user='root', password='@tmsqe!1321', host='127.0.0.1', database='MSO')
+    cnx = mysql.connector.connect(
+        user='root', password='@tmsqe!1321', host='127.0.0.1', database='MSO')
     cur = cnx.cursor(dictionary=True)
 
-    cur.execute("SELECT * FROM users WHERE email = %s", [ email])
+    cur.execute(
+        "SELECT id, first_name, last_name, email, register_date, airport_id, job_title, department FROM users WHERE email = %s",
+        [email])
     r = cur.fetchone()
     cur.close()
     cnx.close()
     return r
 
 
-
 # Register new user into the DB.
 def register_user(sql, data):
-    cnx = mysql.connector.connect(user='root', password='@tmsqe!1321', host='127.0.0.1', database='MSO')
+    cnx = mysql.connector.connect(
+        user='root', password='@tmsqe!1321', host='127.0.0.1', database='MSO')
     cur = cnx.cursor()
     cur.execute(sql, data)
     cnx.commit()
